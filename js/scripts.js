f55bd1f3cec58a3d19b8e038a0e3e4fe63bdee0f
Fan Ding <zichuange@gmail.com>
2017-04-17 22:23:41 +0800

    chapter three arrays, logic and loops

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,22 @@ var answer = prompt(question)
 
 window.alert('You answered ' + answer)
 
+var quiz = [
+  ["What is Superman's real name?", "Clarke Kent"],
+  ["What is Wonderwoman's real name?", 'Dianna Prince'],
+  ["What is Batman's real name?", 'Bruce Wayne']
+]
+
+var score = 0 // initialize score
+var quizNumber = quiz.length
+for (var i = 0; i < quizNumber; i++) {
+  var answer = window.prompt(quiz[i][0])
+  if (answer === quiz[i][1]) {
+    window.alert('Correct!')
+    score++
+  } else {
+    window.alert('Wrong!')
+  }
+}
+
+window.alert('Your score is ' + score)
